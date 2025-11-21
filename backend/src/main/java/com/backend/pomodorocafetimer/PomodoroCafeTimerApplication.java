c0f5f26a172ae2d55c77225ac4582cc5fe1784e7
Alexis Sánchez <xtheggx@gmail.com>
2025-11-21 12:16:51 -0600

    chore: initial backend project setup

---
+++ backend/src/main/java/com/backend/pomodorocafetimer/PomodoroCafeTimerApplication.java
@@ -0,0 +1,13 @@
+package com.backend.pomodorocafetimer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PomodoroCafeTimerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PomodoroCafeTimerApplication.class, args);
+	}
+
+}
